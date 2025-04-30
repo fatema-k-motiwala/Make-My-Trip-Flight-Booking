0a7aa5fa2ec6a863a3329d912327eb74fb84cf14
fkpearl <fkpearl@gmail.com>
2025-04-30 18:22:55 +0400

    Modified the code to select depature and return dates properly

--- src/test/java/FlightBooking.java
+++ src/test/java/FlightBooking.java
@@ -19,6 +19,9 @@ public class FlightBooking {
 
 	@Test
 	public void bookFlight() throws InterruptedException {
+		LocalDate ldDepart = null;
+		LocalDate ldReturn = null;
+
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--incognito");
 		Map<String, Object> prefs = new HashMap<>();
@@ -35,7 +38,6 @@ public class FlightBooking {
 			trip = scanner.nextInt();
 		}
 		scanner.nextLine();
-		
 //		From City & To City
 		System.out.println("Enter From City for flight booking");
 		String customerFromCity = scanner.nextLine();
@@ -45,7 +47,7 @@ public class FlightBooking {
 		System.out.println("Enter depature date in this format: dd/MM/yyyy\n");
 		String depatureDate = scanner.nextLine();
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		LocalDate ldDepart = LocalDate.parse(depatureDate, dtf);
+		ldDepart = LocalDate.parse(depatureDate, dtf);
 		LocalDate today = LocalDate.now();
 		while (ldDepart.isBefore(today)) {
 			System.out.println("Date must be today or later.");
@@ -58,7 +60,7 @@ public class FlightBooking {
 		if (trip == 2) {
 			System.out.println("Enter return date in this format: dd/MM/yyyy\n");
 			String returnDate = scanner.nextLine();
-			LocalDate ldReturn = LocalDate.parse(returnDate, dtf);
+			ldReturn = LocalDate.parse(returnDate, dtf);
 			while (ldReturn.isBefore(today)) {
 				System.out.println("Date must be today or later.");
 				System.out.println("Enter date in this format: dd/MM/yyyy\n");
@@ -81,6 +83,11 @@ public class FlightBooking {
 			// TODO: handle exception
 		}
 
+//		Selecting type of trip
+		if (trip == 1)
+			driver.findElement(By.xpath("//li[@data-cy='oneWayTrip']")).click();
+		else
+			driver.findElement(By.xpath("//li[@data-cy='roundTrip']")).click();
 //      Selecting From City
 		Actions action = new Actions(driver);
 		WebElement fromCity = driver.findElement(By.id("fromCity"));
@@ -90,7 +97,6 @@ public class FlightBooking {
 		inputFromCity.sendKeys(customerFromCity);
 		Thread.sleep(2000);
 		action.sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
-
 //      Selecting To City
 		WebElement toCity = driver.findElement(By.id("toCity"));
 		toCity.click();
@@ -99,32 +105,52 @@ public class FlightBooking {
 		inputToCity.sendKeys(customerToCity);
 		Thread.sleep(2000);
 		action.sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
-
+		Thread.sleep(2000);
+//		Selecting Depature Date
 		String day = String.valueOf(ldDepart.getDayOfMonth());
 		DateTimeFormatter monthFormatter = DateTimeFormatter.ofPattern("MMMM");
-		String month = ldDepart.format(monthFormatter); // Gives "January", "February", etc.
+		String month = ldDepart.format(monthFormatter);
 		String year = String.valueOf(ldDepart.getYear());
-		String reqmonthyr = month + " " + year;
-
-//		Selecting departure date
+		String reqmonthyr = (month + " " + year).replaceAll("\\s+", "");
 		String currentmonthyr = driver.findElement(By.xpath("(//div[@class='DayPicker-Caption'])[1]/div")).getText();
-
-		while (!(currentmonthyr.equals(reqmonthyr))) {
+		while (!((currentmonthyr.replaceAll("\\s+", "")).equals(reqmonthyr))) {
 			driver.findElement(By.xpath("//span[@aria-label='Next Month']")).click();
 			currentmonthyr = driver.findElement(By.xpath("(//div[@class='DayPicker-Caption'])[1]/div")).getText();
 		}
 		List<WebElement> dates = driver
-				.findElements(By.xpath("(//div[@class='DayPicker-Month'])[1]//div[@class='dateInnerCell']/p[1]"));
+				.findElements(By.xpath("//div[@class='DayPicker-Day'][not(contains(@class, 'outside'))]//div[@class='dateInnerCell']/p[1]"));
 		for (WebElement date : dates) {
 			if (date.getText().equals(day)) {
 				date.click();
 				break;
 			}
 		}
+		Thread.sleep(5000);
+//		Selecting Return Date
+		if (trip == 2) {
+			day = String.valueOf(ldReturn.getDayOfMonth());
+			month = ldReturn.format(monthFormatter);
+			year = String.valueOf(ldReturn.getYear());
+			reqmonthyr = (month + " " + year).replaceAll("\\s+", "");
+			currentmonthyr = driver.findElement(By.xpath("(//div[@class='DayPicker-Caption'])[1]/div")).getText();
+			while (!((currentmonthyr.replaceAll("\\s+", "")).equals(reqmonthyr))) {
+				driver.findElement(By.xpath("//span[@aria-label='Next Month']")).click();
+				currentmonthyr = driver.findElement(By.xpath("(//div[@class='DayPicker-Caption'])[1]/div")).getText();
+			}
+			dates = driver
+					.findElements(By.xpath("//div[@class='DayPicker-Day'][not(contains(@class, 'outside'))]//div[@class='dateInnerCell']/p[1]"));
+			for (WebElement date : dates) {
+				if (date.getText().equals(day)) {
+					date.click();
+					break;
+				}
+			}
 
-//            
-//        Thread.sleep(5000);
-//        driver.quit();
+		}
+
+            
+        Thread.sleep(5000);
+        driver.quit();
 
 	}
 }
